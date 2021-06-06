0671c93713e6ec2bac5b949ac9c8f884d09f5c51
Ravina <ravinakeharh1994@gmail.com>
2021-06-06 14:44:36 +0530

    added endpoint

--- src/main/java/com/example/springjenkins/controller/MainController.java
+++ src/main/java/com/example/springjenkins/controller/MainController.java
@@ -3,10 +3,18 @@ package com.example.springjenkins.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 public class MainController {
     @GetMapping("/")
     public String home() {
         return "Hello Jenkins!!!!!!!!!!";
+    }
+
+    @GetMapping("/static-list")
+    public List<String> list() {
+        return Collections.singletonList("item1");
     }
 }
